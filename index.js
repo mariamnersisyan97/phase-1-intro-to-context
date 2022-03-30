6a79413b2dcd793a57038256c79e834ae93abbf2
Mariam Nersisyan <mariamnersisyan97@gmail.com>
2022-03-30 13:50:39 -0400

    commit tree a9eb6f12ba414f37a9f9bc420ea6c48c6cad0066

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Your code here
+function createEmployeeRecord(array) {
+    return {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+// function createEmployeeRecords(array){
+//     let employeeRecs = [];
+//     for(let i = 0; i < array.length; i++){
+//         employeeRecs.push(createEmployeeRecord(array[i]))
+//     }
+// }
+function createEmployeeRecords(array){
+    let newArray = [];
+    for (let i = 0; i < array.length; i++){
+    newArray.push(createEmployeeRecord(array[i]))
+}
+return newArray;
+}
